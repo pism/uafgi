6ff570e336e749ed42584fbcc73e47e7ea58008b
Elizabeth Fischer <elizabeth.fischer@alaska.edu>
2020-12-28 15:37:55 -0900

    Made some changes to move computations out of rules into simple functions.  Probably broke something.

--- uafgi/cdoutil.py
+++ uafgi/cdoutil.py
@@ -1,5 +1,8 @@
+from cdo import Cdo
 import os.path
 from uafgi import ioutil
+import netCDF4
+import cf_units
 
 """Utilities for working with the Python CDO interface"""
 def _large_merge(cdo_merge_operator, input, output, tmp_files, max_merge=30, **kwargs):
@@ -39,7 +42,7 @@ def _large_merge(cdo_merge_operator, input, output, tmp_files, max_merge=30, **k
         cdo_merge_operator(input=input, output=output, **kwargs)
 
 
-def do_merge(cdo_merge_operator, inputs, output, max_merge=30, **kwargs):
+def merge(cdo_merge_operator, inputs, output, max_merge=30, **kwargs):
     """Recursively merge large numbers of files using a CDO merge-type operator.
     Also appropriate for "small" merges.
 
@@ -61,65 +64,39 @@ def do_merge(cdo_merge_operator, inputs, output, max_merge=30, **kwargs):
     with ioutil.TmpFiles(os.path.join(odir, 'tmp')) as tmp_files:
         _large_merge(cdo_merge_operator, inputs, output, tmp_files, max_merge=max_merge, **kwargs)
 
-class merge(object):
-    """Rule for do_merge"""
-
-    def __init__(self, makefile, cdo_merge_operator, inputs, output, max_merge=30, **kwargs):
-        self.inputs = inputs
-        self.output = output
-        self.max_merge = max_merge
-        self.kwargs = kwargs
-        self.cdo_merge_operator = cdo_merge_operator
-        self.rule = makefile.add(self.run,
-            inputs, (output,))
-
-    def run(self):
-        do_merge(self.cdo_merge_operator, self.inputs, self.output, max_merge=self.max_merge, **self.kwargs)
-
 # -------------------------------------------------------------
+def set_time_axis(ifname, ofname, time_bounds, reftime):
+    """Adds time to a NetCDF file; allows for later use with cdo.merge"""
+    cdo = Cdo()
 
-class set_time_axis(object):
-    """Rule to set the time axis for a single file."""
 
-    def __init__(self, makefile, input, output, time_bounds, reftime):
-        """time_boudns (dt0, dt1): [datetime.date]
-            Time bounds for this file
-        """
+    # Set the time axis
+    inputs = [
+        '-setreftime,{}'.format(reftime),  # Somehow reftime is being ignored
+        '{}'.format(ifname)]
+    nominal_date = time_bounds[0] + (time_bounds[1] - time_bounds[0]) / 2
+    cdo.settaxis(
+        nominal_date.isoformat(),
+        input=' '.join(inputs),
+        output=ofname,
+        options="-f nc4 -z zip_2")
 
-        # This deduces the mid-point (nominal) date from the filename
-        self.time_bounds = time_bounds
-        self.nominal_date = time_bounds[0] + (time_bounds[1] - timebounds[0]) / 2
-        self.reftime = reftime
-        self.rule = makefile.add(self.run, (input,), (output,))
+    # Add time bounds --- to be picked up by cdo.mergetime
+    # https://code.mpimet.mpg.de/boards/2/topics/1115
+    with netCDF4.Dataset(ofname, 'a') as nc:
+        nctime = nc.variables['time']
+        timeattrs = [(name,nctime.getncattr(name)) for name in nctime.ncattrs()]
+        nc.variables['time'].bounds = 'time_bnds'
 
-    def run(self):
-        cdo = Cdo()
+        nc.createDimension('bnds', 2)
+        tbv = nc.createVariable('time_bnds', 'd', ('time', 'bnds',))
+        # These attrs don't end up in the final merged time_bnds
+        # But they do seem to be important to keep the final value correct.
+        for name,val in timeattrs:
+            tbv.setncattr(name, val)
 
-        # Set the time axis
-        inputs = [
-            '-setreftime,{}'.format(self.reftime),  # Somehow reftime is being ignored
-            '{}'.format(self.ipath)]
-        cdo.settaxis(
-            self.nominal_date.isoformat(),
-            input=' '.join(inputs),
-            output=self.opath,
-            options="-f nc4 -z zip_2")
+        cfu = cf_units.Unit(nctime.units, nctime.calendar)
+        for ix,time in enumerate(time_bounds):
+            tbv[0,ix] = cfu.date2num(time)
 
-        # Add time bounds --- to be picked up by cdo.mergetime
-        # https://code.mpimet.mpg.de/boards/2/topics/1115
-        with netCDF4.Dataset(self.rule.outputs[0], 'a') as nc:
-            nctime = nc.variables['time']
-            timeattrs = [(name,nctime.getncattr(name)) for name in nctime.ncattrs()]
-            nc.variables['time'].bounds = 'time_bnds'
-
-            nc.createDimension('bnds', 2)
-            tbv = nc.createVariable('time_bnds', 'd', ('time', 'bnds',))
-            # These attrs don't end up in the final merged time_bnds
-            # But they do seem to be important to keep the final value correct.
-            for name,val in timeattrs:
-                tbv.setncattr(name, val)
-
-            cfu = cf_units.Unit(nctime.units, nctime.calendar)
-            for ix,time in enumerate(self.time_bounds):
-                tbv[0,ix] = cfu.date2num(time)
 
--- uafgi/itslive.py
+++ uafgi/itslive.py
@@ -1,44 +1,91 @@
-from uafgi import regrid,cdoutil
+from uafgi import regrid,cdoutil,ioutil,make
 from cdo import Cdo
+import os
+import datetime
 
-# General rule to tranform a single file
-class single_file_transform(object):
-    def __init__(makefile, ifname, ofname, action_fn):
-        self.action_fn = action_fn
-        self.rule = makefile.add(self.run, (input,), (output,)
+def process_year(ifname, year, grid_file, ofname, tdir):
+    """Transforms an Its-Live file from original format to one ready to
+    merge."""
 
-    def run(self):
-        action_fn(self.rule.inputs[0], self.rule.outputs[0])
-
-
-
-def merge_to_pism(makefile, grid, ifpattern, years, odir):
-    """Macro:
-
-    ifpattern:
-        Eg, 'data/itslive/GRE_G0240_{}.nc'
-    years: iterable(int)
-        Years to process
-    """
-
-    # Rule to create each year's file
-    years = list(years)
-    year_files = list()
-    for year in years:
-        syear = '{:04}'.format(year)
-        ifname = ifpattern.format(syear)
-        rule = regrid.extract_region(makefile, grid, ifname,
-            'outputs/{}-grid.nc'.format(grid),
-            ('vx', 'vy', 'v'), 'outputs').rule
-
-        year_files.append(rule.outputs[0])
-
-    # Rule to merge by time
     cdo = Cdo()
-    allyear_file = ifpattern.format('{:04}_{:04}'.format(years[0],years[-1]))
-    xrule = cdoutil.merge(makefile, cdo.mergetime,
-        year_files, allyear_file,
+    odir = os.path.split(ofname)[0]
+
+    # Extract each variable separately
+    merge_files = list()
+    for vname in ('vx', 'vy', 'v'):
+
+        # Use GDAL to extract just local region
+        tmp_v1 = make.opath(ofname, tdir, '_{}1.nc'.format(vname))
+        print('tmp_v1 = {}'.format(tmp_v1))
+        regrid.extract_region(ifname, grid_file, vname, tmp_v1)
+
+        # Add time
+        tmp_v2 = make.opath(ofname, tdir, '_{}2.nc'.format(vname))
+        time_bounds = (datetime.datetime(year,1,1), datetime.datetime(year,12,31))
+        reftime = datetime.date(year,1,1)
+        cdoutil.set_time_axis(tmp_v1, tmp_v2, time_bounds, reftime)
+        
+        # Add to files to merge
+        merge_files.append(tmp_v2)
+
+    # Simple merge
+    print('******** Merging {} -> {}'.format(merge_files, ofname))
+    cdoutil.merge(cdo.merge, merge_files, ofname)
+
+
+def process_years(year_files, years, grid_file, allyear_file, tdir):
+    """Process multiple 1-year Its-Live files into a single file."""
+    cdo = Cdo()
+
+    odir = os.path.split(allyear_file)[0]
+    oyear_files = list()
+
+    for ifname,year in zip(year_files,years):
+        ofname = os.path.join(tdir, 'year_{:04}.nc'.format(year))
+        print('ofname = {}'.format(ofname))
+        process_year(ifname, year, grid_file, ofname, tdir)
+        oyear_files.append(ofname)
+
+    cdoutil.merge(cdo.mergetime, oyear_files, allyear_file,
         options='-f nc4 -z zip_2')
 
-    return xrule
+
+
+class merge_to_pism_rule(object):
+    def __init__(self, makefile, grid, grid_file, ifpattern, years, odir):
+        """Merges a bunch of Its-Live files
+
+        ifpattern:
+            Eg, 'data/itslive/GRE_G0240_{}.nc'
+        years: iterable(int)
+            Years to process
+        """
+
+        # Rule to create each year's file
+        self.grid_file = grid_file
+        self.ifpattern = ifpattern
+        self.years = list(years)
+        self.odir = odir
+
+        iyear_files = [
+            self.ifpattern.format('{:04}'.format(year))
+            for year in years]
+        allyear_file = make.opath(
+            ifpattern.format('{}_{:04}_{:04}'.format(grid, years[0],years[-1])),
+            odir, '')
+
+#        print('allyear_file {}'.format(allyear_file))
+#        print('iyear_files {}'.format('\n'.join(iyear_files)))
+        self.rule = makefile.add(self.run,
+            iyear_files, (allyear_file,))
+
+    def run(self):
+
+        with ioutil.tmp_dir(self.odir,tdir='tdir') as tdir:
+#            oyear_files = [
+#                make.opath(self.ifpattern.format('{:04}'.format(year)), tdir, '_pism')
+#                for year in self.years]
+
+            process_years(self.rule.inputs, self.years, self.grid_file,
+                self.rule.outputs[0], tdir)
 
