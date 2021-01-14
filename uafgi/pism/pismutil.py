5fab0e57e23989b7d933c690725f20362b3e3a44
Elizabeth Fischer <elizabeth.fischer@alaska.edu>
2021-01-13 16:51:12 -0900

    Added comment from Constantine on creating PISM output

--- uafgi/pism/pismutil.py
+++ uafgi/pism/pismutil.py
@@ -8,6 +8,15 @@ def fix_output(output_file3, exception, time_units_s, output_file4):
         Exception (if any) that occurred while running PISM
     units_s: cfunits.Unit
         Units in "seconds since..." format
+
+    NOTE: Fixes to the time unit could be done when the PISM file is opened.
+          https://github.com/pism/pism/blob/master/site-packages/PISM/util.py#L30
+
+          To avoid "fixing" the file you could just copy-and-paste the
+          guts of prepare_output() and supply your own calendar and
+          units string when the file's metadata is written the first
+          time.
+
     """
 
     # Convert to NetCDF
