0ab17bbb9233f765ccc7c5d980b050729cc01f25
Elizabeth Fischer <elizabeth.fischer@alaska.edu>
2021-03-12 19:51:43 -0900

    Added new file

---
+++ uafgi/geotiffutil.py
@@ -0,0 +1,21 @@
+from osgel import gdal
+
+def read(fname, iband):
+    """Reads a GeoTIFF file, returns the raster.
+    fname:
+        Name of GeoTIFF file toread
+    iband: int (1-based)
+        Band in the GeoTIFF file to read
+
+    NOTE: Try inspecting your file with the command:
+        gdalinfo myfile.tif
+    """
+
+    # open dataset
+    ds = gdal.Open(fname)
+    srcband = ds.GetRasterBand(iband)
+    return srcband.ReadAsArray()
+
+    for band in range(1,ds.RasterCount+1):
+        srcband = ds.GetRasterBand(band)
+        arr = srcband.ReadAsArray()
