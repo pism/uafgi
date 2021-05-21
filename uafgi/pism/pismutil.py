94b8d326698cd41d4fab85bdfac15a00be8d7f02
Elizabeth Fischer <elizabeth.fischer@alaska.edu>
2021-05-21 11:40:20 -0800

    Added grid-based flux_across_terminus() method; and also PISM mask enumerated constants.

--- uafgi/pism/pismutil.py
+++ uafgi/pism/pismutil.py
@@ -1,7 +1,19 @@
+import numpy as np
 import os
 import subprocess
 import netCDF4
 from uafgi import ncutil
+
+
+# See PISM source src/util/Mask.hh
+MASK_UNKNOWN          = -1,
+MASK_ICE_FREE_BEDROCK = 0,
+MASK_GROUNDED         = 2,
+MASK_FLOATING         = 3,
+MASK_ICE_FREE_OCEAN   = 4
+
+MULTIMASK_ICE = (MASK_GROUNDED, MASK_FLOATING)
+
 
 def fix_output(output_file3, exception, time_units_s, output_file4, delete_vars=list()):
     """Compress PISM output file while correcting time units; and removes original.
@@ -36,3 +48,59 @@ def fix_output(output_file3, exception, time_units_s, output_file4, delete_vars=
         ncv.attrs['calendar'] = 'proleptic_gregorian'
 
         schema.write(ncin, output_file4)
+
+def flux_across_terminus(mask, fjord, uvel, vvel, dx, dy):
+    """
+    Computes the flux of a vector field across the glacier terminus.
+
+    mask:
+        PISM mask
+    fjord:
+        True where the fjord is
+    uvel, vvel: [m s-1]
+        x and y components of flux
+    dx, dy: [m]
+
+    Returns: fluxmap [m^2 s-1]
+        Raster of flux through each boundary cell.
+        flux = np.sum(np.sum(fluxmap))
+    """
+
+    # Flux to the East: [m^2 s-1] ("horizontal sheet extruding through terminus")
+    maskX = mask.copy()
+    maskX[:,:-1] = mask[:,1:]    # Shift west by 1 pixel
+    fjordX = fjord.copy()
+    fjordX[:,:-1] = fjord[:,1:]    # Shift west by 1 pixel
+    cells = np.logical_and.reduce(
+        (fjord, fjordX, np.isin(mask,MULTIMASK_ICE), maskX==MASK_ICE_FREE_OCEAN))
+    #                  [m s-1]    [0/1]            [m]
+    fluxE = np.maximum(uvel * cells.astype(float) * dy, 0.0)
+
+    # Flux to the West
+    maskX = mask.copy()
+    maskX[:,1:] = mask[:,:-1]    # Shift east by 1 pixel
+    fjordX = fjord.copy()
+    fjordX[:,1:] = fjord[:,:-1]    # Shift east by 1 pixel
+    cells = np.logical_and.reduce(
+        (fjord, fjordX, np.isin(mask,MULTIMASK_ICE), maskX==MASK_ICE_FREE_OCEAN))
+    fluxW = np.maximum(-uvel * cells.astype(float), 0.)
+
+    # Flux to the North
+    maskX = mask.copy()
+    maskX[:-1,:] = mask[1:,:]    # Shift south by 1 pixel
+    fjordX = fjord.copy()
+    fjordX[:-1,:] = fjord[1:,:]    # Shift south by 1 pixel
+    cells = np.logical_and.reduce(
+        (fjord, fjordX, np.isin(mask,MULTIMASK_ICE), maskX==MASK_ICE_FREE_OCEAN))
+    fluxN = np.maximum(vvel * cells.astype(float) * dx, 0.0)
+
+    # Flux to the South
+    maskX = mask.copy()
+    maskX[1:,:] = mask[:-1,:]    # Shift north by 1 pixel
+    fjordX = fjord.copy()
+    fjordX[1:,:] = fjord[:-1,:]    # Shift north by 1 pixel
+    cells = np.logical_and.reduce(
+        (fjord, fjordX, np.isin(mask,MULTIMASK_ICE), maskX==MASK_ICE_FREE_OCEAN))
+    fluxS = np.maximum(-vvel * cells.astype(float) * dx, 0.0)
+
+    return fluxE + fluxW + fluxN + fluxS
