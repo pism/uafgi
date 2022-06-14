676ecd23f21ecf14f8cc4156b5c6cb7f1b804339
Elizabeth Fischer <eafischer2@alaska.edu>
2022-06-14 12:09:08 -0400

    Reorganized files

--- uafgi/quantutil.py
+++
@@ -1,7 +0,0 @@
-# https://stackoverflow.com/questions/14313510/how-to-calculate-rolling-moving-average-using-numpy-scipy
-def moving_average(x, w):
-
-    """w:
-        Width of moving average (integer number of points).
-        Assumes equispaced data."""
-    return np.convolve(x, np.ones(w), 'valid') / w
