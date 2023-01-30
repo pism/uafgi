e20196b4c6387fa05796cadd62e413531e360862
Chris Waigl <chris.waigl@gmail.com>
2023-01-30 14:22:40 -0900

    spelling correction

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='uafgi',
     version='0.0.1',
     url='https://github.com/pism/uafgi',
-    author='Elizabeth Fisher',
+    author='Elizabeth Fischer',
     author_email='eafischer2@alaska.edu',
     description='Some tools for internal use in in research at UAF/GI',
     packages=find_packages(),    
