33f80dfa6b17d5c5d91f2d8cba414cb6718704ce
eafischer2 <eafischer2@alaska.edu>
2023-11-08 15:24:11 -0900

    changes

--- uafgi/util/shputil.py
+++ uafgi/util/shputil.py
@@ -512,7 +512,9 @@ def write_df(df, shape_col, shapely_type, ofname, wkt=None, zip_format=False):
         #print('field_defs: {}, {}, {}'.format(cname, df1[cname].dtype, dtype2ogr[df1[cname].dtype]))
         #print('field_defs: {}, {}'.format(cname, df1[cname].dtype))
         print('df1[cname] = ', cname, df1[cname].dtype)
-        field_defs.append((cname, dtype2ogr[df1[cname].dtype]))
+        dtype = df1[cname].dtype
+        ogrtype = dtype2ogr[dtype]
+        field_defs.append((cname, ogrtype))
 
     # print('field_defs ',field_defs)
     with shputil.ShapefileWriter(ofname, shapely_type, field_defs, wkt=wkt, zip_format=zip_format) as writer:
