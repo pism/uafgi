e86887bd0db41796482da3ad37e481bda93067ff
eafischer2 <eafischer2@alaska.edu>
2023-12-13 09:36:12 -0900

    changes

--- uafgi/util/pathutil.py
+++ uafgi/util/pathutil.py
@@ -65,10 +65,10 @@ class RootsDict:
 #        path = os.path.abspath(os.path.realpath(syspath))#.replace(os.sep, '/')
         for _,key in self.sorted:
             root = self.lookup[key]
-            print('root: ', str(root))
-            print('parents: ', str(path))
-            if str(path).startswith(str(root)):
-#            if root in path.parents:
+#            print('root: ', str(root))
+#            print('parents: ', str(path))
+#            if str(path).startswith(str(root)):
+            if root in path.parents:
                 return pathlib.PurePosixPath('{'+key+'}') / path.relative_to(root)
         ret = pathlib.PurePosixPath(path)
 
