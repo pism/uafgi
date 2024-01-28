fde6fe3768cb1fd14bb1a3af8b08de71805d299b
eafischer2 <eafischer2@alaska.edu>
2024-01-27 18:25:27 -0900

    changes

--- uafgi/util/ioutil.py
+++ uafgi/util/ioutil.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import contextlib
+import contextlib,pathlib
 import sys
 import os,subprocess
 import re
