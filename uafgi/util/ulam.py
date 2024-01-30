93bcd5cfde54d50c60dc34a4c7bc741dfe648b61
eafischer2 <eafischer2@alaska.edu>
2024-01-29 16:40:17 -0900

    Added Ulam Sprial

---
+++ uafgi/util/ulam.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+# Implements the Ulam Spiral
+# It's a nice way to assign increasing 1D numbers to gridcells near a center cell.
+
+
+def n_to_xy(n):
+    """Ufunc"""
+    sqrt_n = np.sqrt(n)
+    m = int(np.floor(sqrt_n))
+    if int(np.floor(2*sqrt_n))%2 == 0:
+        k1x = n-m*(m+1)
+        k1y = 0
+    else:
+        k1x = 0
+        k1y = n-m*(m+1)
+
+    sgn = int(pow(-1,m))
+    k2 = int(np.ceil(.5*m))
+
+    x = sgn * (k1x + k2)
+    y = sgn * (k1y - k2)
+    return (x,y)
+
+def xy_to_n(xy):
+    x,y = xy
+    sgn = -1 if x<y else 1
+    k = max(np.abs(x), np.abs(y))
+
+    n = (4*k*k) + sgn * (2*k + x + y)
+
+    return n
+
